e5d2dc5798f9efaf8d4a8e8670b0575fb7d24a2a
alphasquadcsus <alphasquadron42@gmail.com>
2015-11-03 10:15:44 -0800

    added edit functionality

--- public/javascripts/quizHelper.js
+++ public/javascripts/quizHelper.js
@@ -2,11 +2,9 @@
  * Created by Edgar on 10/14/15.
  */
 var newQuiz;
-var questionCount = 0;
 var space = "&nbsp; &nbsp; &nbsp; &nbsp;";
+var questionCount = 0;
 $(document).ready(function(){
-    //quiz object for creating a new quiz
-
     //HTML DOM Objects
     var jQuiz = $('#quiz');
     var jQuestions = $('#questions');
@@ -56,28 +54,162 @@ $(document).ready(function(){
     }
 
     function addQuestion(){
-		var questionName = $('#newQuestion').val();
-
+		var i = 0;
+        var questionName = $('#newQuestion').val();
         var arrQuestion = [];
-        arrQuestion[0] = $('#answer1').val();
-        arrQuestion[1] = $('#answer2').val();
-        arrQuestion[2] = $('#answer3').val();
-        arrQuestion[3] = $('#answer4').val();
-        arrQuestion[4] = $('#answer5').val();
-
-        var answer = $('#answerKey').val();
-        newQuiz.addQuestion(questionName, arrQuestion, answer, questionCount);
-		questionCount++; //Increment question number answer array is for
-        $('#tempQuestion').remove();
-        jQuiz.append("<br><b>" + questionName +":</b><br>");
-		jQuiz.append(space, arrQuestion[0]+"<br>");
-		jQuiz.append(space, arrQuestion[1]+"<br>");
-		jQuiz.append(space, arrQuestion[2]+"<br>");
-		jQuiz.append(space, arrQuestion[3]+"<br>");
-		jQuiz.append(space, arrQuestion[4]+"<br>");
 		
+		if( $('#newQuestion').val() != '' && $('#answerKey').val() != ''){
+		
+		} else{
+		
+		}
+		
+		if( !($('#answer1').val() == '')) {
+			arrQuestion[i] = $('#answer1').val();
+			i++;
+		}
+		
+		if( !($('#answer2').val() == '')) {
+			arrQuestion[i] = $('#answer2').val();
+			i++;
+		}
+		
+		if( !($('#answer3').val() == '')) {
+			arrQuestion[i] = $('#answer3').val();
+			i++;
+		}
+			
+		if( !($('#answer4').val() == '')) {
+			arrQuestion[i] = $('#answer4').val();
+			i++;
+		}
+				
+		if( !($('#answer5').val() == '')) {
+			arrQuestion[i] = $('#answer5').val();
+			i++;
+		}
+			
+        var answer = $('#answerKey').val();
+        newQuiz.addQuestion(questionName, arrQuestion, answer);
+        $('#tempQuestion').remove();
+		var $questionDiv = $("<div>", {id: "q"+questionCount});
+		$questionDiv.append("<br><b>" + questionName +":</b>" + space + "<input type='button' id='question"+questionCount+"' value='Edit'><br>");
+		
+		for(var i = 0; i < arrQuestion.length; i++) {
+			$questionDiv.append(space, (i+1) +") " + arrQuestion[i]+"<br>");
+		}
+		$questionDiv.append("Answer: " + answer +"<br>");
+		jQuiz.append($questionDiv);
+		editQuestion();
         generateQATemplate();
     };
+	
+	function editQuestion(){
+		var temp = "question" + questionCount++;
+		$('#'+temp).click(function(){
+			editTemplate(temp);
+		});
+		
+	}
+	
+	function editTemplate(question){
+		var index = question.match(/(\d\d)|(\d)/g);
+		$('#tempQuestion').remove();
+		var temp = newQuiz.getQuestions();
+		var question = temp[index];
+		temp = newQuiz.getAnswers();
+		var answers = temp[index];
+		for(var i = 0; i < 5; i++){
+			if(typeof(answers[i]) == 'undefined'){
+				answers[i] = "";
+			}
+		}
+		temp = newQuiz.getAnswerKeys();
+		var answerKey = temp[index];
+		jQuestions.append(
+            "<div id='tempQuestion'>" +
+            "Question:<input type='text' id='newQuestion' value='"+question+"'>"+
+            "<br>"+
+            "Possible Answers:<br>"+
+            "Answer 1: <input type='text' id='answer1' value='"+answers[0]+"'>" +
+            "<br>" +
+            "Answer 2: <input type='text' id='answer2' value='"+answers[1]+"'>" +
+            "<br>" +
+            "Answer 3: <input type='text' id='answer3' value='"+answers[2]+"'>" +
+            "<br>" +
+            "Answer 4: <input type='text' id='answer4' value='"+answers[3]+"'>" +
+            "<br>" +
+            "Answer 5: <input type='text' id='answer5' value='"+answers[4]+"'>" +
+            "<br>" +
+            "<br>" +
+            "Answer key #: <input type='text' id='answerKey' value='"+answerKey+"'>"+
+            "<br><br>" +
+            "<input type='button' id='submitEdit' value='Submit Edit'>" + space +
+			"<input type='button' id='cancelEdit' value='Cancel Edit'>" +
+            "</div>"
+        );
+
+		$('#submitEdit').click(function(){
+			submitEdit(index);
+		});
+
+		$('#cancelEdit').click(function(){
+			$('#tempQuestion').remove();
+			generateQATemplate();
+		});
+	}
+
+	function submitEdit(index){
+		var i = 0;
+        var questionName = $('#newQuestion').val();
+        var arrQuestion = [];
+		
+		if( $('#newQuestion').val() != '' && $('#answerKey').val() != ''){
+		
+		} else{
+		
+		}
+		
+		if( !($('#answer1').val() == '')) {
+			arrQuestion[i] = $('#answer1').val();
+			i++;
+		}
+		
+		if( !($('#answer2').val() == '')) {
+			arrQuestion[i] = $('#answer2').val();
+			i++;
+		}
+		
+		if( !($('#answer3').val() == '')) {
+			arrQuestion[i] = $('#answer3').val();
+			i++;
+		}
+			
+		if( !($('#answer4').val() == '')) {
+			arrQuestion[i] = $('#answer4').val();
+			i++;
+		}
+				
+		if( !($('#answer5').val() == '')) {
+			arrQuestion[i] = $('#answer5').val();
+			i++;
+		}
+			
+        var answer = $('#answerKey').val();
+        newQuiz.addQuestion(questionName, arrQuestion, answer, index);
+        $('#tempQuestion').remove();
+		var temp = "q" + index;
+		var $questionDiv = $('#'+temp);
+		$questionDiv.empty();
+		$questionDiv.append("<br><b>" + questionName +":</b>" + space + "<input type='button' id='question"+questionCount+"' value='Edit'><br>");
+		
+		for(var i = 0; i < arrQuestion.length; i++) {
+			$questionDiv.append(space, (i+1) +") " + arrQuestion[i]+"<br>");
+		}
+		$questionDiv.append("Answer: " + answer +"<br>");
+		editQuestion();
+        generateQATemplate();
+	}
 	
 	function submitQuiz(){
 		//Ajax by Dennis
@@ -98,6 +230,5 @@ $(document).ready(function(){
 		});
 		window.location.replace('/viewQuizzes2'); //redirect 
 	};
-
-
+	}
 });
