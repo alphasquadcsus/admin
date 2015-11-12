581cd5a502ef06402126e8dd98dbf5f969bff8a4
edgar-m <edgar.morales916@gmail.com>
2015-11-12 09:51:18 -0800

    added tour type
    a user can now select which categories the tour falls under

--- html/_js/createTour.js
+++ html/_js/createTour.js
@@ -3,7 +3,27 @@ $(document).ready(function(){
 		var name = $('#name').val();
 		var lat = $('#latitude').val();
 		var longt = $('#longitude').val();
+		var tourType = []
 		
+		if($('#general').is(':checked'))
+			tourType.push('general');
 		
+		if($('#concrete').is(':checked'))
+			tourType.push('concrete');
+		
+		if($('#timber').is(':checked'))
+			tourType.push('timber');
+		
+		if($('#connections').is(':checked'))
+			tourType.push('connections');
+		
+		if($('#steel').is(':checked'))
+			tourType.push('steel')
+		
+		if($('#lateral').is(':checked'))
+			tourType.push('lateral');
+		
+		if($('#short').is(':checked'))
+			tourType.push('short');
 	});
 });
