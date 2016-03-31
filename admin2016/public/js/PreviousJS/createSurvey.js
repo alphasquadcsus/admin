6577ab8ea43163fe79127f7e3268df77ae6461c7
dpoon1146 <dpoon@ymail.com>
2016-03-31 11:28:48 -0700

    New admin, without old admin things

--- admin2016/public/js/PreviousJS/createSurvey.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$('#submit').click(function(){
-		var name = $('#name').val();
-		var lat = $('#latitude').val();
-		var longt = $('#longitude').val();
-		
-		
-	});
-});
