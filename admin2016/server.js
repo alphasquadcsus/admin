713323c0547dd812b336ae9b53120f2d7b425f80
dpoon1146 <dpoon@ymail.com>
2016-05-14 18:24:43 -0700

    Removed old files and uploaded finished admin site

--- admin2016/server.js
+++ admin2016/server.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var path = require('path');
-var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+var session = require('express-session');
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+
+//Public variable for AdminUser Mongodb model
+AdminUser = require('./server/models/adminUser.js');
 
 var config = require('./config/serverConfig');
-var routes = require('./server/routes/index');
+var routes = require('./server/routes/index.js');
 
 var app = express();
-
 
 //Mongoose setup
 mongoose.connect(config.db, function(err){
@@ -19,20 +27,30 @@ mongoose.connect(config.db, function(err){
 	}
 });
 
-
-//Morgan
+//Environments
 app.use(morgan('dev'));
-//Body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: false 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use(session({
+	secret: 'adminSecured',
+	resave: false,
+	saveUninitialized: false
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Set public folder as static path
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Passport config
+passport.use(new localStrategy(AdminUser.authenticate()));
+passport.serializeUser(AdminUser.serializeUser());
+passport.deserializeUser(AdminUser.deserializeUser());
+
 //Routes
-app.use('/api', routes); //API routing
+app.use('/api', routes); 
 app.get('*', function(req,res){
 	res.sendFile('index.html');
 });
@@ -44,5 +62,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+//Start server on port
 app.listen(config.port);
 console.log("Server listening on port " + config.port +"\n");
